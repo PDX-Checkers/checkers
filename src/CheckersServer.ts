75882c2136c0a7cdbbfcb7b9027d730b6bb8b206
Ryan Lanigan <ryan.lanigan@seeq.com>
2019-11-04 11:30:40 -0800

    Cleaned up spacing and debug comments

--- src/CheckersServer.ts
+++ src/CheckersServer.ts
@@ -4,17 +4,16 @@ import { Server } from '@overnightjs/core';
 import { Logger } from '@overnightjs/logger';
 
 class CheckersServer extends Server {
-  
+
   constructor() {
     super(true);
     this.app.use(express.json());
     this.app.use(express.urlencoded({extended: true}));
     this.setupControllers();
-  } 
+  }
 
   private setupControllers(): void {
     const ctlrInstances = [];
-    debugger;
     for (const name in controllers) {
         if (controllers.hasOwnProperty(name)) {
             const controller = (controllers as any)[name];
@@ -27,7 +26,7 @@ class CheckersServer extends Server {
   public start(port: number): void {
     this.app.get('*', (req, res) => {
       res.send('Server\'s up. Port: ' + port);
-  });
+    });
     this.app.listen(port, () => {
       Logger.Info('Server\'s up. Port: ' + port);
     })
