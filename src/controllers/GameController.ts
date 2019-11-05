6ae463fbe93dc8908e36fc688327884e1475d04c
Michael Bottini <mbottini1@gmail.com>
2019-11-04 21:22:22 -0800

    Complete a proof of concept with querying server
    Assume that the following apply:
    
    * A database user "blarg" with password "blarg" can connect to
    the `checkers` table of the MariaDB instance.
    * The `checkers` table contains a `users` table.
    
    This will be able to query the server.

--- src/controllers/GameController.ts
+++ src/controllers/GameController.ts
@@ -1,16 +1,33 @@
 import { Request, Response } from 'express';
 import { Controller, Get } from '@overnightjs/core';
 import { Logger } from '@overnightjs/logger';
+import * as mariadb from "mariadb";
 
 
 @Controller('api/game')
 export class GameController {
 
+  private queryUsers(): Promise<object> {
+    const pool = mariadb.createPool({
+      user: "blarg",
+      password: "blarg",
+      database: "checkers",
+      connectionLimit: 5
+    });
+    return pool.getConnection()
+               .then(conn => 
+                conn.query("SELECT * FROM users"))
+               .then()
+               .catch(err => err);
+  }
+
   @Get()
   private getGameState(req: Request, res: Response) {
     Logger.Info(req.params.msg);
-    res.status(200).json({
-      message: 'Beans'
-    })
+    this.queryUsers()
+        .then(result => 
+          res.status(200).json({
+            message: result
+          }));
   }
 }
